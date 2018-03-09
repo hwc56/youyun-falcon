c6712423e0dbd1839384c73968fd00866036a831
hwc56 <1203023336@qq.com>
2018-03-09 16:42:41 +0800

    three commit

---
+++ sys/ntp/push-data.sh
@@ -0,0 +1,1 @@
+ts=`date +%s`; curl -X POST -d "[{\"metric\": \"hwcjjj\", \"endpoint\": \"172-16-30-119\", \"timestamp\": $ts, \"step\": 60,\"value\": 9,\"counterType\": \"GAUGE\",\"tags\": \"project=falcon,module=webapp\"}]" http://192.168.6.145:1988/v1/push
