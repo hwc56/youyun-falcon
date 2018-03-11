f2e4cec841eb832e830ad7510a5e91568e623554
StupidBirdFly <1203023336@qq.com>
2018-03-11 09:53:45 +0800

    Delete 30_data.sh
    no used

--- sys/ntp/30_data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-
-ts=`date +%s`; curl -X POST -d "[{\"metric\": \"testmetricme.second\", \"endpoint\": \"testhost\", \"timestamp\": $ts, \"step\": 60,\"value\": 20,\"counterType\": \"GAUGE\",\"tags\": \"project=falcon,module=webapp\"}]" http://192.168.6.145:1988/v1/push
